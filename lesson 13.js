0b44d7e24db95d90ce08815234b96c140f6deea2
nick <xaockharkov@gmail.com>
2022-02-23 11:30:49 +0200

    lesson13.js file

---
+++ lesson 13.js
@@ -0,0 +1,36 @@
+"use strict";
+
+if (1) {
+ console.log ('ok!');
+} else {
+     console.log ('Error');
+}
+
+
+
+//if (num < 49) {
+  //console.log('Error');
+//} else if (num > 100) {
+//    console.log ('Много');
+//} else {
+ //   console.log ('Ok');
+//}
+//(num === 50) ? console.log('Ok!') : console.log ('Error');
+
+const num = '50';
+
+ switch (num) {
+     case '49':
+         console.log('No true');
+         break;
+     case '100':
+         console.log ('No true');
+         break;
+ default:
+     console.log ('Not now');
+ }
+     //case 50:
+         // console.log ('True');
+          //break;
+        
+ 
